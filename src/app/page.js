be198aa9c6d78e6bfa1e32499b47df0cccd2fb59
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2024-10-21 10:55:13 -0800

    change

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+ 
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -21,6 +23,21 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
+      <h1>tanzil</h1>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
